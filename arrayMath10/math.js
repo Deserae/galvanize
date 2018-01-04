c92c82b66782cef9d2525c3d5a5c2c0f4f48e9e2
Deserae <deseraedias@gmail.com>
2018-01-03 22:59:52 -0700

    adding final lesson 10 exercise

---
+++ arrayMath10/math.js
@@ -0,0 +1,38 @@
+var arr = [1,4,7];
+
+function sum(arr) {
+  var sum = 0;
+  for (var i = 1; i < arr.length; i++) {
+   sum += arr[i];
+  }
+  console.log(sum);
+  return sum;
+}
+
+function product(arr) {
+  // YOUR CODE HERE
+}
+
+function filterPassingGrades(arr) {
+  // YOUR CODE HERE
+}
+
+function replace(arr, from, to) {
+  // YOUR CODE HERE
+}
+
+function max(arr) {
+  // YOUR CODE HERE
+}
+
+function min(arr) {
+  // YOUR CODE HERE
+}
+
+function mean(arr) {
+  // YOUR CODE HERE
+}
+
+function median(arr) {
+  // YOUR CODE HERE
+}
