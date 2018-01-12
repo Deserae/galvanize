eb96ec2815c09757d3126de0e50c02882603a450
Deserae <deseraedias@gmail.com>
2018-01-11 22:02:30 -0700

    initial commit - challenge 14 function

---
+++ challenge14/index.js
@@ -0,0 +1,29 @@
+function highestSum (ccArray) {
+	for (var i = 0; i < ccArray.length; i++){
+//remove dashes, split apart, loop over and add add, set to new array?
+//or
+//split apart, loop over and add while ignoring dashes,set to new array
+	}
+
+}
+
+var ccArray = [
+'4916-2600-1804-0530', //49
+'4779-252888-3972', //81
+'4252-278893-7978', //81
+'4556-4242-9283-2260'] //64
+
+highestSum(ccArray);
+
+//need to add each item in array
+//--remove dashes
+//--break string into indivual pieces
+//--add together
+
+//compare added numbers
+//--if number is the greatest then save, 
+//  if number is the same as greatest,
+//  should return the last one
+
+//return the original item in array
+//--use index?
